62564c6eb3fb5ec0b7cef7bcfe085001e2f72d06
Alisha Khatri <alisha.191502@ncit.edu.np>
2023-12-10 21:59:32 +0545

    home

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Box } from '@mui/material';
+import theme from '../assets/theme.png'
+
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+    return (
+        <Box width="100%" height="100%" margin="auto">
+        <Box width="100%" padding={0}>
+            <img src={theme} alt="" style={{ width: '100%', height: "500px", objectFit: 'cover' }} />
+        </Box>
+    </Box>
+    )
 }
 
-export default HomePage
+export default HomePage;
